aa4f24dafb8dfff4e8f244f3220571c7d31d2908
Rodrigo <rodrigorael53@gmail.com>
2023-02-16 00:52:34 -0300

    :sparkles: feat: create order event

---
+++ src/bounded-contexts/sales/order-created.ts
@@ -0,0 +1,19 @@
+import { DomainEvent } from "../../core/DomainEvent";
+import { Order } from "./order";
+
+// essa classe é um evento
+export class OrderCreatedEvent implements DomainEvent {
+  public createdAt: Date;
+  public order: Order;
+
+  constructor(order: Order) {
+    this.createdAt = new Date();
+    this.order = order;
+  }
+
+  // para criar o método getEntityId eu preciso salvar dentro da classe(evento)
+  // qual entidade está relacionado com esse evento
+  getEntityId(): string {
+    return this.order.id;
+  }
+}
